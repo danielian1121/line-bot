69adaf3cca8980672abce6c615cc23bceddf630b
danielian1121 <danielian1121@gmail.com>
2018-10-23 16:56:53 +0800

    PM2.5

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const linebot = require('linebot')
 const express = require('express')
+const rp = require('request-promise')
+
+const SITE_NAME = '臺南'
+const apiOpt = {
+  url: 'http://opendata2.epa.gov.tw/AQI.json',
+  json: true
+}
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
@@ -11,6 +18,17 @@ const app = express()
 
 const linebotParser = bot.parser()
 
+function readResult(result) {
+  let data
+  for (i in repos) {
+      if (repos[i].SiteName === SITE_NAME) {
+          data = repos[i];
+          break;
+      }
+  }
+  return data
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,12 +36,27 @@ app.get('/', (req, res) => {
 app.post('/webhook', linebotParser)
 
 bot.on('message', event => {
-  event.reply(event.message.text)
-    .then(data => {
-      console.log('successs', data)
-    }).catch(error => {
-      console.log('error', error)
-    })
+  switch (event.message.type) {
+    case 'text':
+      switch (event.message.text) {
+        case '空氣':
+          let data
+          rp(apiOpt)
+            .then(result => {
+              data = readResult(result)
+              event.reply(data.County + data.SiteName +
+                '\n\nPM2.5指數：' + data['PM2.5_AVG'] +
+                  '\n狀態：' + data.Status)
+            }).catch(() => {
+              event.reply('無法取得空氣品質資料～')
+            })
+          break
+      }
+      break
+    default:
+      event.reply('Unknow message: ' + JSON.stringify(event))
+      break
+  }
 })
 
 app.listen(process.env.PORT || 80, () => {
